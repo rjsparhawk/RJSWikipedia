4fefed67d866e75ea32cf6882398a677f6ce8d57
Robert Sparhawk <rjsparhawk@gmail.com>
2025-08-24 13:49:10 -0400

    initial commit

---
+++ RJSWikipedia/RJSWikipediaApp.swift
@@ -0,0 +1,17 @@
+//
+//  RJSWikipediaApp.swift
+//  RJSWikipedia
+//
+//  Created by Robert Sparhawk on 8/24/25.
+//
+
+import SwiftUI
+
+@main
+struct RJSWikipediaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ RJSWikipediaTests/RJSWikipediaTests.swift
@@ -0,0 +1,16 @@
+//
+//  RJSWikipediaTests.swift
+//  RJSWikipediaTests
+//
+//  Created by Robert Sparhawk on 8/24/25.
+//
+
+import Testing
+
+struct RJSWikipediaTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
